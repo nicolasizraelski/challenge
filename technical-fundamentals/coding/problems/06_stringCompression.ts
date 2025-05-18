8ac9adb24277a85c7b351528fd50dc21643836a3
Nicolas Izraelski <130162382+nicolasizraelski@users.noreply.github.com>
2025-05-17 21:45:01 -0300

    Update 06_stringCompression.ts

--- technical-fundamentals/coding/problems/06_stringCompression.ts
+++ technical-fundamentals/coding/problems/06_stringCompression.ts
@@ -6,6 +6,24 @@
 // your method should return the original string.
 // You can assume the string has only uppercase and lowercase letters (a - z).
 
-export default function stringCompression (str: string) : string {
- 
+export default function stringCompression(str: string): string {
+  let char = str[0];
+  let times = 1;
+  let compressed = "";
+
+  for (let i = 1; i < str.length; i++) {
+    const curr = str[i];
+
+    if (curr !== char) {
+      compressed += char + times;
+      char = curr;
+      times = 1;
+    } else {
+      times++;
+    }
+  }
+
+  compressed += char + times;
+
+  return compressed.length < str.length ? compressed : str;
 }
