0c1c79084d2cc56f2f77d8827a9a00e95eeff850
Nicolas Izraelski <130162382+nicolasizraelski@users.noreply.github.com>
2025-05-17 21:44:37 -0300

    Update 04_palindromePermutation.ts

--- technical-fundamentals/coding/problems/04_palindromePermutation.ts
+++ technical-fundamentals/coding/problems/04_palindromePermutation.ts
@@ -1,4 +1,4 @@
-// 4. *Palindrome Permutation*: 
+// 4. *Palindrome Permutation*:
 
 // Given a string, write a function to check if it is a permutation of a palindrome.
 // A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters.
@@ -9,6 +9,35 @@
 // Output True (permutations: "taco cat", "atco cta", etc.)
 // ```
 
-export default function palindromePermutation (str: string): boolean {
+export default function palindromePermutation(str: string): boolean {
+  const firstOcurrencies: Array<{ char: string; times: number }> = [];
 
+  str
+    .toLocaleLowerCase()
+    .replaceAll(" ", "")
+    .split("")
+    .forEach((char) => {
+      const index = firstOcurrencies.findIndex((occ) => occ.char == char);
+
+      if (index !== -1) {
+        firstOcurrencies[index].times++;
+      } else {
+        firstOcurrencies.push({ char, times: 1 });
+      }
+    });
+
+  let oddCount = 0;
+
+  for (let i = 0; i < firstOcurrencies.length; i++) {
+    const current = firstOcurrencies[i];
+    if (current.times % 2 != 0) {
+      oddCount++;
+    }
+
+    if (oddCount > 1) {
+      return false;
+    }
+  }
+
+  return true;
 }
