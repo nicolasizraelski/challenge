a8e0fa4102c17b702629cd71975b27bd7150b448
Nicolas Izraelski <130162382+nicolasizraelski@users.noreply.github.com>
2025-05-17 21:44:14 -0300

    Update 02_checkPermutations.ts

--- technical-fundamentals/coding/problems/02_checkPermutations.ts
+++ technical-fundamentals/coding/problems/02_checkPermutations.ts
@@ -2,4 +2,32 @@
 
 // Given two strings, write a method to decide if one is a permutation of the other.
 
-export default function checkPermutations(s1: string, s2: string): boolean {}
+export default function checkPermutations(s1: string, s2: string): boolean {
+  if (s2.length != s1.length) return false;
+  const firstOcurrencies: Array<{ char: string; times: number }> = [];
+
+  s1.split("").forEach((char) => {
+    const index = firstOcurrencies.findIndex((occ) => occ.char == char);
+
+    if (index !== -1) {
+      firstOcurrencies[index].times++;
+    } else {
+      firstOcurrencies.push({ char, times: 1 });
+    }
+  });
+
+  for (let i = 0; i < s2.split("").length; i++) {
+    const char = s2[i];
+    const index = firstOcurrencies.findIndex((occ) => occ.char == char);
+
+    if (index === -1) {
+      return false;
+    } else if (firstOcurrencies[index].times == 0) {
+      return false;
+    } else {
+      firstOcurrencies[index].times--;
+    }
+  }
+
+  return true;
+}
